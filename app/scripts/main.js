709299e98f7fc4859965a2959a6560e8787fa25a
mseabourne <mseabourne@hotmail.com>
2014-04-06 21:57:59 -0700

    Working on including globalize with multiple languages. Not yet working properly with loading modules.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
 console.log('\'Allo \'Allo!');
+
+Globalize.loadTranslation( "en-US", {
+  greetings: {
+    hello: "howdy folks",
+    bye: "bye"
+  }
+});
+
+Globalize.loadTranslation( "en-CA", {
+  greetings: {
+    hello: "how's it going, eh!",
+    bye: "see ya later hoser"
+  }
+});
+$( document ).ready(function() {
+
+  $("#countrySelect").change(function() {
+    alert("switch country");
+  });
+  
+});
