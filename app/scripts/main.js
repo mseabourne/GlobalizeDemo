8a5530c37ce73fcbd2c27f47869868f02d7a55e8
mseabourne <mseabourne@hotmail.com>
2014-04-08 22:17:07 -0700

    Final touches and added fr culture.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,8 +3,6 @@
 
 "use strict";
 
-console.log('\'Allo \'Allo!');
-
 Globalize.addCultureInfo( 'en-US', {
     messages: {
         buttonHome: 'Home',
@@ -12,7 +10,8 @@ Globalize.addCultureInfo( 'en-US', {
         buttonContact: 'Contact',
         welcomeHeader: 'Hello',
         welcomeMessage: 'America is all about speed. Hot, nasty, badass speed. -Eleanor Roosevelt, 1936',
-        btnSuccess: 'Go'
+        btnSuccess: 'Go',
+        btnSuccessDestination: 'http://allrecipes.com/Recipe/Baked-Potato/Detail.aspx'
     }
 });
 
@@ -23,18 +22,42 @@ Globalize.addCultureInfo( 'en-CA', {
         buttonContact: 'Contact, eh!',
         welcomeHeader: 'Hello, eh!',
         welcomeMessage: 'Hey, we found a dead mouse in our beer, eh? That means you owe us a free case',
-        btnSuccess: 'Go'
+        btnSuccess: 'Get it',
+        btnSuccessDestination: 'http://allrecipes.com/Recipe/Real-Poutine/Detail.aspx'
+    }
+});
+
+Globalize.addCultureInfo( 'fr-FR', {
+    messages: {
+        buttonHome: 'Maison',
+        buttonAbout: 'Sur',
+        buttonContact: 'Contacter',
+        welcomeHeader: 'Bonjour',
+        welcomeMessage: 'Tour de France commence 5th Juillet',
+        btnSuccess: 'Aller',
+        btnSuccessDestination: 'http://allrecipes.fr/recette/7165/moules-frites.aspx'
     }
 });
 
 
 var myGlobalizeDemo = myGlobalizeDemo || {};
 myGlobalizeDemo.switchCountry = function(lang){
-    $("#btnHome").text(Globalize.localize( 'buttonHome', lang ));
-    $("#btnAbout").text(Globalize.localize( 'buttonAbout', lang ));
-    $("#btnContact").text(Globalize.localize( 'buttonContact', lang ));
-    $("#welcomeHeader").text(Globalize.localize( 'welcomeHeader', lang ));
-    $("#welcomeMessage").text(Globalize.localize( 'welcomeMessage', lang ));
+    
+    // Update individual elements
+    // $("#btnHome").text(Globalize.localize( 'buttonHome', lang ));
+    // $("#btnAbout").text(Globalize.localize( 'buttonAbout', lang ));
+    // $("#btnContact").text(Globalize.localize( 'buttonContact', lang ));
+    // $("#welcomeHeader").text(Globalize.localize( 'welcomeHeader', lang ));
+    // $("#welcomeMessage").text(Globalize.localize( 'welcomeMessage', lang ));
+    Globalize.culture(lang);
+    
+    // Update all based on data attributes    
+    $('[data-localize]').each(function(index) {
+        $(this).text(Globalize.localize($(this).data('localize')));
+    });
+
+    // example of culture time formatting
+    $('#currentTime').text(Globalize.format(new Date(), 'F'));
 };
 
 
@@ -43,5 +66,7 @@ $( document ).ready(function() {
   $('#countrySelect').change(function() {
     myGlobalizeDemo.switchCountry($(this).val());
   }).trigger('change');
-  
+  $('#btnSuccess').click(function() {
+    window.open(Globalize.localize('btnSuccessDestination'));
+  });
 });
