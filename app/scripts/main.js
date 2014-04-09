8be06cfc05d31f5d02f57abd441fabd22ac7e4c3
mseabourne <mseabourne@hotmail.com>
2014-04-08 21:25:18 -0700

    Integrate globalize.js and hookup two languages.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,47 @@
+/* jshint indent: false */
+/*jshint -W109 */ // disable single quote warning
+
+"use strict";
+
 console.log('\'Allo \'Allo!');
 
-Globalize.loadTranslation( "en-US", {
-  greetings: {
-    hello: "howdy folks",
-    bye: "bye"
-  }
+Globalize.addCultureInfo( 'en-US', {
+    messages: {
+        buttonHome: 'Home',
+        buttonAbout: 'About',
+        buttonContact: 'Contact',
+        welcomeHeader: 'Hello',
+        welcomeMessage: 'America is all about speed. Hot, nasty, badass speed. -Eleanor Roosevelt, 1936',
+        btnSuccess: 'Go'
+    }
 });
 
-Globalize.loadTranslation( "en-CA", {
-  greetings: {
-    hello: "how's it going, eh!",
-    bye: "see ya later hoser"
-  }
+Globalize.addCultureInfo( 'en-CA', {
+    messages: {
+        buttonHome: 'Home, eh!',
+        buttonAbout: 'About, eh!',
+        buttonContact: 'Contact, eh!',
+        welcomeHeader: 'Hello, eh!',
+        welcomeMessage: 'Hey, we found a dead mouse in our beer, eh? That means you owe us a free case',
+        btnSuccess: 'Go'
+    }
 });
+
+
+var myGlobalizeDemo = myGlobalizeDemo || {};
+myGlobalizeDemo.switchCountry = function(lang){
+    $("#btnHome").text(Globalize.localize( 'buttonHome', lang ));
+    $("#btnAbout").text(Globalize.localize( 'buttonAbout', lang ));
+    $("#btnContact").text(Globalize.localize( 'buttonContact', lang ));
+    $("#welcomeHeader").text(Globalize.localize( 'welcomeHeader', lang ));
+    $("#welcomeMessage").text(Globalize.localize( 'welcomeMessage', lang ));
+};
+
+
 $( document ).ready(function() {
 
-  $("#countrySelect").change(function() {
-    alert("switch country");
-  });
+  $('#countrySelect').change(function() {
+    myGlobalizeDemo.switchCountry($(this).val());
+  }).trigger('change');
   
 });
